23f3c31d9a0fa981f3dd7b84b0a79d205de8175c
tonnymuchui <tonykanyingah@gmail.com>
2023-01-09 15:09:57 +0300

    modify App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Development
         </a>
       </header>
     </div>
