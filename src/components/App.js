c30b565c7c0dc2bff3ab7e9b5b939f85bc5c0666
tonnymuchui <tonykanyingah@gmail.com>
2023-01-10 22:45:31 +0300

    Add App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
