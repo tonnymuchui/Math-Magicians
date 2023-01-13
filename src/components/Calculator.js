56952fd4a8c7991a25c79133a72c86d3bcf252d6
tonnymuchui <tonykanyingah@gmail.com>
2023-01-13 14:13:42 +0300

    refactor calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,35 +12,38 @@ class Calculator extends Component {
     const { total, next, operation } = this.state;
     return (
 
-      <div className="app">
+      <div className="App">
         <div className="calculator">
           <div className="display">
             { total}
             {operation}
             {next}
           </div>
-          <div className="operators">
-            <button type="button" color="primary" value="">&#247;</button>
-            <button type="button" color="primary" value="">&times;</button>
-            <button type="button" color="primary" value="">-</button>
-            <button type="button" color="primary" value="">+</button>
-            <button type="button" color="primary" value="">=</button>
-          </div>
 
-          <div className="numbers">
-            <button type="button" color="primary" name="AC">AC</button>
-            <button type="button" color="primary" name="">+/-</button>
-            <button type="button" color="primary" name="">%</button>
-            <button type="button" color="primary" name="2">2</button>
-            <button type="button" color="primary" name="3">3</button>
-            <button type="button" color="primary" name="4">4</button>
-            <button type="button" color="primary" name="5">5</button>
-            <button type="button" color="primary" name="6">6</button>
-            <button type="button" color="primary" name="7">7</button>
-            <button type="button" color="primary" name="8">8</button>
-            <button type="button" color="primary" name="9">9</button>
-            <button type="button" color="primary" name="0">0</button>
-            <button type="button" color="primary" name=".">.</button>
+          <div className="operators">
+            <button type="button" className="press-btn">AC</button>
+            <button type="button" className="press-btn">+/-</button>
+            <button type="button" className="press-btn">%</button>
+            <button type="button" className="press-btn amber">÷</button>
+
+            <button type="button" className="press-btn-1">7</button>
+            <button type="button" className="press-btn-1">8</button>
+            <button type="button" className="press-btn-1">9</button>
+            <button type="button" className="press-btn amber">X</button>
+
+            <button type="button" className="press-btn">4</button>
+            <button type="button" className="press-btn">5</button>
+            <button type="button" className="press-btn">6</button>
+            <button type="button" className="press-btn amber">-</button>
+
+            <button type="button" className="press-btn">1</button>
+            <button type="button" className="press-btn">2</button>
+            <button type="button" className="press-btn">3</button>
+            <button type="button" className="press-btn amber">+</button>
+
+            <button type="button" className="press-btn-2">0</button>
+            <button type="button" className="press-btn">.</button>
+            <button type="button" className="press-btn amber">=</button>
           </div>
         </div>
       </div>
