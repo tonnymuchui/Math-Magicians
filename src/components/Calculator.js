acabe81607bf1366b8f0507bfff8c82add0bcb0b
tonnymuchui <tonykanyingah@gmail.com>
2023-01-13 12:03:30 +0300

    add calculator component render

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Calculator.css';
 
-function Calculstor() {
-  return (
-    <>
-      <h1>Test Tonny</h1>
-    </>
-  );
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+
+      <div className="calculate-app">
+        <div className="calculator-container">
+          <div className="display">
+            { total}
+            {operation}
+            {next}
+          </div>
+          <div className="operators">
+            <button type="button" color="primary" value="">&#247;</button>
+            <button type="button" color="primary" value="">&times;</button>
+            <button type="button" color="primary" value="">-</button>
+            <button type="button" color="primary" value="">+</button>
+            <button type="button" color="primary" value="">=</button>
+          </div>
+
+          <div className="numbers">
+            <button type="button" color="primary" name="AC">AC</button>
+            <button type="button" color="primary" name="">+/-</button>
+            <button type="button" color="primary" name="">%</button>
+            <button type="button" color="primary" name="2">2</button>
+            <button type="button" color="primary" name="3">3</button>
+            <button type="button" color="primary" name="4">4</button>
+            <button type="button" color="primary" name="5">5</button>
+            <button type="button" color="primary" name="6">6</button>
+            <button type="button" color="primary" name="7">7</button>
+            <button type="button" color="primary" name="8">8</button>
+            <button type="button" color="primary" name="9">9</button>
+            <button type="button" color="primary" name="0">0</button>
+            <button type="button" color="primary" name=".">.</button>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
 }
-export default Calculstor;
+export default Calculator;
