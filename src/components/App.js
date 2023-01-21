236a8f510c581876a7c61098f4e504f96d33c51b
tonnymuchui <tonykanyingah@gmail.com>
2023-01-21 13:23:12 +0300

    Update App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import Calculator from './Calculator';
+import Home from './Home';
+import Header from './Navbar';
+import Quotes from './Quotes';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Quotes" element={<Quotes />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 export default App;
